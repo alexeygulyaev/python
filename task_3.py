193ec3ac2c8c69ed6830464d7d043849f635d2ce
Alexey Gulyaev <alexeygulyaev7@gmail.com>
2022-12-18 14:19:06 +0700

    first commit

---
+++ task_3.py
@@ -0,0 +1,20 @@
+# Задание: Напишите функцию is_one_away(word1, word2), которая принимает в качестве аргументов два слова word1 и word2 и
+# возвращает значение True если слова имеют одинаковую длину и отличаются ровно в 1 символе и False в противном случае.
+
+def is_one_away(word1, word2):
+    count = 0
+    if len(word1) == len(word2):
+        for i in range(len(word1)):
+            if word1[i] == word2[i]:
+                continue
+            else:
+                count += 1
+    if count == 1:
+        return True
+    else:
+        return False
+
+txt1 = input()
+txt2 = input()
+
+print(is_one_away(txt1, txt2))
